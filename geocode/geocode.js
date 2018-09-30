d28b138477bdb5e79ef8ec20adfec69a7733faee
kentenroth <kentencroth@yahoo.com>
2018-09-30 17:25:17 -0500

    Initial Setup
    First commit

---
+++ geocode/geocode.js
@@ -0,0 +1,6 @@
+const request = require('request')
+const API_KEYS = require('./../../../API_KEYS/API_KEYS')
+
+const mapquest_key = API_KEYS.mapquest_key
+
+console.log(mapquest_key)
---
+++ weather/weather.js
@@ -0,0 +1,4 @@
+const request = require('request')
+const API_KEYS = require('./../../../API_KEYS/API_KEYS')
+
+const darkSky = API_KEYS.dark_sky_key
