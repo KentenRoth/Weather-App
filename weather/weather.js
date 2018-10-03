cc3cf17482eac88d92a831e19851707f956a564c
kentenroth <kentencroth@yahoo.com>
2018-10-03 12:53:57 -0500

    Setup getWeather function
    Setup the main function for getWeather.  Still need to add in the else if parts and what is returned.

--- weather/weather.js
+++ weather/weather.js
@@ -8,3 +8,22 @@ const darkSky = API_KEYS.dark_sky_key
 
 // current, feels like, summary, humidity, % chance of rain.
 // get upcoming forcast daily summary and % chance of rain each day.
+
+var getWeather = (lat, lng, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/6a4ff104399321633b67f86e38ba966c/${lat},${lng}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Cannot reach servers')
+        } else if () {
+            callback('')
+        } else if () {
+            callback(undefined, {
+
+            })
+        }
+    })
+}
+
+module.exports.getWeather = getWeather
